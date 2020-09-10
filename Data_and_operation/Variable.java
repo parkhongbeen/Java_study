ad02326ff4f9bae0915e0082fd4d959c17705772
parkhongbeen <pack122@naver.com>
2020-09-11 02:02:36 +0900

    변수

---
+++ Data_and_operation/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+	public static void main(String[] args) {
+		int a = 1; // Number -> integer ... -2, -1, 0, 1 ...
+		System.out.println(a);
+		
+		double b = 1.1; // real number -> double ... -2.0, -1.0, 0, 1.0 ...
+		System.out.println(b);
+		
+		String c = "Hello World";
+		System.out.println(c);
+	}
+}
+
+// Java는 변수를 선언할 때 선언할 변수안에 형태를 지정해줘야 
