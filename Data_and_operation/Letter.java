0cc25a728441285c2d25d11a35f3545ba7288527
parkhongbeen <pack122@naver.com>
2020-09-11 02:39:41 +0900

    변수2

---
+++ Data_and_operation/Letter.java
@@ -0,0 +1,12 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		String friend1 = "hong";
+		String friend2 = "been";
+		System.out.println("Hello, "+friend1+"..."+friend2+".........bye");
+		
+		double VAT = 10.0;
+		System.out.println("부가 가치세는"+VAT);
+	}
+}
