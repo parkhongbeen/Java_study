cf9997426fe9c2a96c22c7e9335c0967eef2428c
parkhongbeen <pack122@naver.com>
2020-09-14 18:12:45 +0900

    반복문(while)

--- MyApp/AccountingArrayLoopApp.java
+++ MyApp/AccountingArrayLoopApp.java
@@ -1,5 +1,5 @@
  
-public class AccountingArrayApp {
+public class AccountingArrayLoopApp {
 
 	public static void main(String[] args) {
 		
@@ -12,23 +12,22 @@ public class AccountingArrayApp {
 		double income = valueOfSupply - expense;
 		
 		
-		double[] dividendRates = new double[3];
-		dividendRates[0] = 0.5;
-		dividendRates[1] = 0.3;
-		dividendRates[2] = 0.2;
-		
-		double dividend1 = income * dividendRates[0];
-		double dividend2 = income * dividendRates[1];
-		double dividend3 = income * dividendRates[2];
 		
 		System.out.println("Value of supply : " + valueOfSupply);
 		System.out.println("VAT : " + vat );
 		System.out.println("Total : " + total );
 		System.out.println("Expense : " + expense );
 		System.out.println("Income : " + income);
-		System.out.println("Dividend :" + dividend1);
-		System.out.println("Dividend :" + dividend2);
-		System.out.println("Dividend :" + dividend3);
+		
+		double[] dividendRates = new double[3];
+		dividendRates[0] = 0.5;
+		dividendRates[1] = 0.3;
+		dividendRates[2] = 0.2;
+		
+		int i = 0;
+		while(i < dividendRates.length) {
+		System.out.println("Dividend :" + (income * dividendRates[i]));
+		i = i + 1;
+		}
 	}
-
 }
