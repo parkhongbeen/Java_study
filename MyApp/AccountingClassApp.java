fe623c857be78c7ee8968dd6eec0d38b7a49f9ad
parkhongbeen <pack122@naver.com>
2020-09-14 19:28:52 +0900

    인스턴스

--- MyApp/AccountingClassApp.java
+++ MyApp/AccountingClassApp.java
@@ -1,9 +1,9 @@
 class Accounting{
-	public static double valueOfSupply;
-	public static double vatRate;
-	public static double expenseRate;
+	public double valueOfSupply;
+	public double vatRate;
+	public double expenseRate;
 	
-	static void print() {
+	public void print() {
 		System.out.println("Value of supply : " + valueOfSupply);
 		System.out.println("VAT : " + getVAT() );
 		System.out.println("Total : " + getTotal() );
@@ -14,42 +14,51 @@ class Accounting{
 		System.out.println("Dividend :" + getDividend3());
 	}
 
-	private static double getDividend1() {
+	public double getDividend1() {
 		return getIncome() * 0.5;
 	}
 	
-	private static double getDividend2() {
+	public double getDividend2() {
 		return getIncome() * 0.3;
 	}
 	
-	private static double getDividend3() {
+	public double getDividend3() {
 		return getIncome() * 0.2;
 	}
 
-	private static double getIncome() {
+	public double getIncome() {
 		return valueOfSupply - getExpense();
 	}
 
-	private static double getExpense() {
+	public double getExpense() {
 		return valueOfSupply * expenseRate;
 	}
 
-	private static double getTotal() {
+	public double getTotal() {
 		return valueOfSupply + getVAT();
 	}
 
-	private static double getVAT() {
+	public double getVAT() {
 		return valueOfSupply * vatRate;
 	}
 }
 
+
 public class AccountingClassApp {
 
 	public static void main(String[] args) {
-		Accounting.valueOfSupply = 10000.0;
-		Accounting.vatRate = 0.1;
-		Accounting.expenseRate = 0.3;
-		Accounting.print();
+		// instance
+		Accounting a1 = new Accounting();
+		a1.valueOfSupply = 20000.0;
+		a1.vatRate = 0.1;
+		a1.expenseRate = 0.3;
+		a1.print();
+		
+		Accounting a2 = new Accounting();
+		a2.valueOfSupply = 30000.0;
+		a2.vatRate = 0.1;
+		a2.expenseRate = 0.2;
+		a2.print();
 	}
 
 }
